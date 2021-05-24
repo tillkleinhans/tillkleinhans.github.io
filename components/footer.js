53c84e7cc1c770dc1aac97538c73e569de99d4e6
tillkleinhans <tillklh@gmail.com>
2021-05-24 19:26:18 +0200

    started landing page case, improved components

--- components/footer.js
+++ components/footer.js
@@ -3,6 +3,29 @@ footerTemplate.innerHTML = `
     <style>
         footer {
             text-align: center;
+            margin-bottom: 70px;
+        }
+
+        footer a {
+            padding: 0px 20px;
+            text-decoration: none;
+            color: #212121;
+        }
+
+        .footer-links {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+        }
+
+        .icon-group {
+            display: flex;
+        }
+
+        .icon {
+            width: 25px;
+            height: 25px;
+            margin-right: 5px;
         }
 
         @media only screen and (max-width: 600px) {
@@ -14,7 +37,21 @@ footerTemplate.innerHTML = `
     <footer>
         <h2>Let's connect</h2>
         <p>Get in touch for opportunities or just say hi 👋</p>
-        <a href="https://www.linkedin.com/in/till/" target="_blank">My LinkedIn profile</a>
+        <div class="footer-links">
+            <a href="mailto:hi@tillkleinhans.com">
+                <div class="icon-group">
+                    <img class="icon" src="./source/icon-email.svg" />
+                    hi@tillkleinhans.com
+                </div>
+            </a>
+            <a href="https://www.linkedin.com/in/till/" target="_blank">
+                <div class="icon-group">
+                    <img class="icon" src="./source/icon-linkedin.svg" />
+                    LinkedIn
+                </div>
+            </a>
+            <a>Imprint</a>
+        </div>
     </footer>
 `;
 
