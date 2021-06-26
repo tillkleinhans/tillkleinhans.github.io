daf00059c67cd0cd770c732582753a5ab66ab7d5
tillkleinhans <tillklh@gmail.com>
2021-06-26 14:46:54 +0200

    hackathon teaser improved

--- components/header.js
+++ components/header.js
@@ -1,5 +1,3 @@
-const headerTemplate = document.createElement('template'); //TODO: remove if unnecessary
-
 class Header extends HTMLElement {
     constructor() {
         super();
@@ -10,7 +8,7 @@ class Header extends HTMLElement {
             <header>
                 <div class="section-content">
                     <a class="title" href="./index.html">
-                        <span class="text-block">Till Kleinhans<div class="desktop-only"> – </div></span>
+                        <span class="text-block">Till Kleinhans <div class="desktop-only">– </div></span>
                         <span class="text-block desktop-only">Product Designer</span>
                     </a>
                 </div>
